8df12acd1f9be1c44687e0464e2ae423e1ddb00e
anthonybrend <anthony@abrend.com>
2015-11-14 21:42:56 +0300

    remove _DEV_, _PROD_ from define plugin

--- config/core.js
+++ config/core.js
@@ -21,9 +21,7 @@ module.exports = {
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-            },
-            _DEV_        : JSON.stringify(process.env.NODE_ENV === 'development'),
-            _PROD_       : JSON.stringify(process.env.NODE_ENV === 'production')
+            }
         }),
         new webpack.ProvidePlugin({
             React    : 'react',
