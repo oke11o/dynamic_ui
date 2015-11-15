43b06cb1341bb84e643cf22e7c3eeb9811063869
anthonybrend <anthony@abrend.com>
2015-11-15 15:02:22 +0300

    added basic layout:TODO

--- app/views/layout/index.js
+++ app/views/layout/index.js
@@ -1,12 +1,29 @@
 import './styles.scss'
-import {Styles, AppBar, IconButton, Icons} from 'material-ui'
-
-const ThemeManager = Styles.ThemeManager;
+import {Styles, AppBar, Menu, MenuItem} from 'material-ui'
+import mui from 'material-ui'
+const ThemeManager  = Styles.ThemeManager;
 const LightRawTheme = Styles.LightRawTheme;
+
+var AppBarStyles = {backgroundColor: Styles.Colors.blue600};
+var MenuStyles = {
+    borderTop: "none",
+    overflow: "hidden",
+    position: "absolute",
+    top: "64px",
+    width: "192px"
+};
+var ContentStyles = {
+    borderLeft: "solid 1px #e0e0e0",
+    boxSizing: "border-box",
+    marginLeft: "192px",
+    maxWidth: "896px",
+    minHeight: "800px",
+    padding: "24px 24px 24px 24px"
+};
 
 class App extends React.Component {
 
-    constructor () {
+    constructor() {
         super();
 
         this.state = {
@@ -18,20 +35,34 @@ class App extends React.Component {
         muiTheme: React.PropTypes.object
     };
 
-    getChildContext () {
+    getChildContext() {
         return {
             muiTheme: this.state.muiTheme
         }
     }
 
     render() {
+        console.log(mui);
+
+
         return (
             <div {...this.props}>
                 <AppBar
+                    style={AppBarStyles}
                     title="Title"
-                    iconElementLeft={null}>
-
-                </AppBar>
+                    zDepth={0}/>
+                <div style={ContentStyles}>
+                    s
+                </div>
+                <div style={MenuStyles}>
+                    <Menu
+                        menuItems={[{route:'/asd', text: 'ss'}]}
+                        autoWidth={true}
+                        hideable={false}
+                        visible={true}
+                        zDepth={0}>
+                    </Menu>
+                </div>
             </div>
         )
     }
