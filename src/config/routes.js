aae051f5d6e1cbe4d0189bf997c6b4c331c20b46
anthonybrend <anthony@abrend.com>
2015-11-16 03:06:58 +0300

    todo routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -6,20 +6,38 @@ import configureStore from 'store/configureStore'
 
 import Layout from 'views/layout'
 import Login from 'views/pages/auth'
-import Dynamic from 'views/pages/dynamic'
 
 const store = configureStore();
 
+const ROUTE_CONFIG = {
+    component  : 'div',
+    childRoutes: [
+        {
+            path: '/login',
+            component: Login
+        }, {
+            component  : Layout,
+            onEnter (nextState, replaceState) {
+                console.log(nextState);
+            },
+            childRoutes: [
+                {
+                    path     : "*",
+                    getComponent (location, cb) {
+                        require.ensure([], (require) => {
+                            cb(null, require('views/pages/dynamic').default)
+                        })
+                    }
+                }
+            ]
+        }
+    ]
+};
 
 export function start() {
     ReactDOM.render(
         <Provider store={store}>
-            <Router history={createBrowserHistory()}>
-                <Route path="/login" component={Login}/>
-                <Route component={Layout}>
-                    <Route path="*" component={Dynamic}/>
-                </Route>
-            </Router>
+            <Router history={createBrowserHistory()} routes={ROUTE_CONFIG}/>
         </Provider>,
         document.getElementById('app')
     )
